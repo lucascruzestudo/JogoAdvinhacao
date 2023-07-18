cfa9fdd54f26af757bfeaaacf3dca60ae3e9ab1b
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-18 16:49:17 -0300

    Melhorias no funcionamento do jogo e mudança de tentativas e range de números

--- advinhacao.py
+++ advinhacao.py
@@ -2,12 +2,15 @@ import random as rd
 
 def numero_usuario():
     while True:
-        entrada = input("Digite um numero: ")
+        entrada = input("Digite um numero (1-50): ")
         try:
             entrada = int(entrada)
-            return entrada
+            if (entrada >= 1 and entrada <= 50):
+                return entrada
+            else:
+                print("O número secreto está entre 1 e 50!\n")
         except:
-            print("Valor informado invalido!\n")
+             print("Valor informado invalido!\n")
 
 def verificar_entrada(entrada, numero_secreto):
     acerto = (entrada == numero_secreto)
@@ -29,18 +32,20 @@ print("*********************************")
 print("Bem vindo ao jogo de advinhação!!")
 print("*********************************\n")
 
-tentativas = 10
-numero_secreto = rd.randint(1,100)
+tentativas = 5
+numero_secreto = rd.randint(1,50)
 
 while tentativas > 0:
     print(f"Você possui {tentativas} tentativas.")
     entrada = numero_usuario()
     acertou, auxiliar = verificar_entrada(entrada, numero_secreto)
     if acertou:
-        print("Parabéns, você acertou!\n")
+        print(f"Parabéns, você acertou! O número secreto era {numero_secreto}.\n")
         break
     else:
         print(f"Você errou, o número secreto é {auxiliar} do que o número informado.\n")
     tentativas -= 1
+    if (tentativas == 0):
+        print(f"Acabaram suas tentativas, o número secreto era {numero_secreto}.")
 
 print("Fim de jogo!")
