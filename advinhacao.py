49fd1df91326408bed37a27aa696806ffb0806ab
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-18 13:51:08 -0300

    revisão da lógica de acerto e erro visando padronização

--- advinhacao.py
+++ advinhacao.py
@@ -1,7 +1,5 @@
 import random as rd
 
-numero_secreto = rd.randint(1,100)
-
 def numero_usuario():
     while True:
         entrada = input("Digite um numero: ")
@@ -12,35 +10,39 @@ def numero_usuario():
             print("Valor informado invalido!\n")
 
 def verificar_entrada(entrada, numero_secreto):
-    print("\nVocê digitou: ", entrada)
     acerto = (entrada == numero_secreto)
 
     if acerto:
 
-        print("Você acertou!\n")
-        return True
+        return True, None
 
     else:
 
-        maior = (entrada > numero_secreto)
+        maior = (numero_secreto > entrada)
 
         if maior:
-            print("Você errou, o número secreto é menor do que o que você digitou!\n")
+            return False, "MAIOR"
         else:
-            print("Você errou, o número secreto é maior do que o que você digitou!\n")
+            return False, "MENOR"
 
-        return False
 
 
 print("*********************************")
 print("Bem vindo ao jogo de advinhação!!")
 print("*********************************\n")
 
+tentativas = 10
+numero_secreto = rd.randint(1,100)
 
-
-while True:
+while tentativas > 0:
+    print(f"Você possui {tentativas} tentativas.")
     entrada = numero_usuario()
-    encerramento = verificar_entrada(entrada, numero_secreto)
-    if encerramento == True:
-        print("Fim de jogo!")
+    acertou, auxiliar = verificar_entrada(entrada, numero_secreto)
+    if acertou:
+        print("Parabéns, você acertou!\n")
         break
+    else:
+        print(f"Você errou, o número secreto é {auxiliar} do que o número informado.\n")
+    tentativas -= 1
+
+print("Fim de jogo!")
