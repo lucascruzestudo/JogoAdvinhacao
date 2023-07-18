5281ebd559e6bdaaae65c33f7497f7de8c70591c
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-18 12:20:44 -0300

    jogo pronto permitindo que o usuário possa realizar várias tentativas

--- advinhacao.py
+++ advinhacao.py
@@ -1,36 +1,44 @@
 numero_secreto=32
 
-def entrada_usuario():
+def numero_usuario():
     while True:
-        tentativa = input("Digite um numero: ")
+        entrada = input("Digite um numero: ")
         try:
-            tentativa = int(tentativa)
-            return tentativa
+            entrada = int(entrada)
+            return entrada
         except:
             print("Valor informado invalido!\n")
+
+def verificar_entrada(entrada, numero_secreto):
+    print("\nVocê digitou: ", entrada)
+    acerto = (entrada == numero_secreto)
+
+    if acerto:
+
+        print("Você acertou!\n")
+        return True
+
+    else:
+
+        maior = (entrada > numero_secreto)
+
+        if maior:
+            print("Você errou, o número secreto é menor do que o que você digitou!\n")
+        else:
+            print("Você errou, o número secreto é maior do que o que você digitou!\n")
+
+        return False
 
 
 print("*********************************")
 print("Bem vindo ao jogo de advinhação!!")
 print("*********************************\n")
 
-tentativa = entrada_usuario()
-
-print("\nVocê digitou: ", tentativa)
-
-acerto = tentativa == numero_secreto
-
-if acerto:
-    print("Você acertou!\n")
-
-else:
-
-    maior = tentativa > numero_secreto
-
-    if maior:
-        print("Você errou, o número secreto é menor do que o que você digitou!\n")
-    else:
-        print("Você errou, o número secreto é maior do que o que você digitou!\n")
 
 
-print("Fim de jogo!")
+while True:
+    entrada = numero_usuario()
+    encerramento = verificar_entrada(entrada, numero_secreto)
+    if encerramento == True:
+        print("Fim de jogo!")
+        break
