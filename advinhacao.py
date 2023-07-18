b60d092d356ca91073ee23038d731cacb8b382cb
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-17 21:38:36 -0300

    input do usuário e resultado final simples

--- advinhacao.py
+++ advinhacao.py
@@ -1,1 +1,22 @@
-print('Bem vindo ao jogo de advinhação!!')
+numero_secreto=32
+
+print("*********************************")
+print("Bem vindo ao jogo de advinhação!!")
+print("*********************************\n")
+
+while True:
+    tentativa = input("Digite um numero: ")
+    try:
+        tentativa = int(tentativa)
+        break
+    except:
+        print("Valor informado invalido!\n")
+
+print("Você digitou", tentativa)
+
+if numero_secreto == tentativa:
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
+print("Fim de jogo!")
