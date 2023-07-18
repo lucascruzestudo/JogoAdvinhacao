5b83146f1cdfa4cdce8922dc9d46c89f31d3856b
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-17 22:11:46 -0300

    função para processamento de input, melhoria de legibilidade

--- advinhacao.py
+++ advinhacao.py
@@ -1,22 +1,36 @@
 numero_secreto=32
 
+def entrada_usuario():
+    while True:
+        tentativa = input("Digite um numero: ")
+        try:
+            tentativa = int(tentativa)
+            return tentativa
+        except:
+            print("Valor informado invalido!\n")
+
+
 print("*********************************")
 print("Bem vindo ao jogo de advinhação!!")
 print("*********************************\n")
 
-while True:
-    tentativa = input("Digite um numero: ")
-    try:
-        tentativa = int(tentativa)
-        break
-    except:
-        print("Valor informado invalido!\n")
+tentativa = entrada_usuario()
 
-print("Você digitou", tentativa)
+print("\nVocê digitou: ", tentativa)
 
-if numero_secreto == tentativa:
-    print("Você acertou!")
+acerto = tentativa == numero_secreto
+
+if acerto:
+    print("Você acertou!\n")
+
 else:
-    print("Você errou!")
+
+    maior = tentativa > numero_secreto
+
+    if maior:
+        print("Você errou, o número secreto é menor do que o que você digitou!\n")
+    else:
+        print("Você errou, o número secreto é maior do que o que você digitou!\n")
+
 
 print("Fim de jogo!")
